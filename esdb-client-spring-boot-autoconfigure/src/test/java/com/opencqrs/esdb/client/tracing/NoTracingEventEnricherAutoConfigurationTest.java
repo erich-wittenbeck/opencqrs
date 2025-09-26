63d99ee60f69a1e658bbf49fb36feaa7da13c28e
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2025-09-26 12:42:10 +0200

    test: test autoconfig conditions properly

--- esdb-client-spring-boot-autoconfigure/src/test/java/com/opencqrs/esdb/client/tracing/NoTracingEventEnricherAutoConfigurationTest.java
+++ esdb-client-spring-boot-autoconfigure/src/test/java/com/opencqrs/esdb/client/tracing/NoTracingEventEnricherAutoConfigurationTest.java
@@ -2,14 +2,10 @@
 package com.opencqrs.esdb.client.tracing;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
 
-import io.opentelemetry.api.OpenTelemetry;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.actuate.autoconfigure.opentelemetry.OpenTelemetryAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 public class NoTracingEventEnricherAutoConfigurationTest {
@@ -19,7 +15,6 @@ public class NoTracingEventEnricherAutoConfigurationTest {
     @Test
     public void noTracingEventEnricherUsed() {
         runner.withConfiguration(AutoConfigurations.of(NoTracingEventEnricherAutoConfiguration.class))
-                .withClassLoader(new FilteredClassLoader(OpenTelemetry.class, OpenTelemetryAutoConfiguration.class))
                 .run(context -> {
                     assertThat(context).hasNotFailed().hasSingleBean(NoTracingEventEnricher.class);
                 });
@@ -28,15 +23,9 @@ public class NoTracingEventEnricherAutoConfigurationTest {
     @Test
     public void otherTracingEventEnricherFoundAndUsed() {
         runner.withConfiguration(AutoConfigurations.of(OpenTelemetryEventEnricherAutoConfiguration.class))
-                .withClassLoader(new FilteredClassLoader(OpenTelemetry.class, OpenTelemetryAutoConfiguration.class))
-                .withBean(
-                        TracingEventEnricher.class, NoTracingEventEnricherAutoConfigurationTest::mockOtherEventEnricher)
+                .withBean(TracingEventEnricher.class, Mockito::mock)
                 .run(context -> {
                     assertThat(context).hasNotFailed().doesNotHaveBean(NoTracingEventEnricher.class);
                 });
-    }
-
-    private static TracingEventEnricher mockOtherEventEnricher() {
-        return mock(TracingEventEnricher.class, Mockito.RETURNS_DEEP_STUBS);
     }
 }
